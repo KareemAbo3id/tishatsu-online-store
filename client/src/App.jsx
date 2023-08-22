26d17afd2ee9879d212d4b6bc63dfd27070b70d1
Kareem Aboueid <kareemabo3id@hotmail.com>
2023-08-22 20:25:47 +0300

    changed some directories and re-branded, added LICENSE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { Helmet } from 'react-helmet';
 import Header from './components/Header.component';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
   return (
     <main className="vh-100" style={{ fontSize: '15px' }}>
+      <Helmet>
+        <title>tishatsu - online store</title>
+      </Helmet>
       <Header />
       <ToastContainer />
       <Outlet />
--- client/src/components/Header.component.jsx
+++ client/src/components/Header.component.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>MERN AUTH APP</Navbar.Brand>
+            <Navbar.Brand>Tishatsu</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
